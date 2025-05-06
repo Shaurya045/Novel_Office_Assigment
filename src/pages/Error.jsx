a1b6a2979eb913066562ed49b987c837c6630d45
Shaurya Pratap Singh <shauryasinghad01@gmail.com>
2025-05-06 20:49:06 +0530

    made the error and notfound pages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-[10px] md:gap-[20px] h-full items-center justify-center px-[20px]">
+      <p className="text-xl sm:text-2xl md:text-4xl font-normal text-center">
+        Something went wrong in the application
+      </p>
+      <Link to="/">
+        <Button variant="outlined">GO HOME</Button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
-function NotFoundPage() {
+const NotFoundPage = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen flex flex-col">
+      <div className="flex-1 flex items-center justify-center p-4">
+        <div className="max-w-md w-full text-center">
+          <h1 className={`text-9xl font-bold mb-4 `}>404</h1>
+          <h2 className={`text-3xl font-bold mb-4 `}>Page Not Found</h2>
+          <Link to="/">
+            <Button variant="outlined">GO HOME</Button>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
